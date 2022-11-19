7b5738d70b2a951f3807b1ac9b63bb7e5321f562
Ardian <ardianfirmansyah123@gmail.com>
2022-11-19 15:11:45 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -6,3 +6,137 @@ import numpy as np
 
 data = pd.read_csv("https://raw.githubusercontent.com/krishnaik06/Diabetes-Prediction/master/data/pima-data.csv")
 print(data.shape)
+print(data.head())
+print(data.tail())
+data.isnull().values.any()
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+#get correlations of each features in dataset
+corrmat = data.corr()
+top_corr_features = corrmat.index
+plt.figure(figsize=(20,20))
+#plot heat map
+g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+
+data.corr()
+diabetes_map = {True: 1, False: 0}
+data['diabetes'] = data['diabetes'].map(diabetes_map)
+data.head(5)
+diabetes_true_count = len(data.loc[data['diabetes'] == True])
+diabetes_false_count = len(data.loc[data['diabetes'] == False])
+(diabetes_true_count,diabetes_false_count)
+
+## Train Test Split
+
+from sklearn.model_selection import train_test_split
+feature_columns = ['num_preg', 'glucose_conc', 'diastolic_bp', 'insulin', 'bmi', 'diab_pred', 'age', 'skin']
+predicted_class = ['diabetes']
+
+X = data[feature_columns].values
+y = data[predicted_class].values
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state=10)
+print("total number of rows : {0}".format(len(data)))
+print("number of rows missing glucose_conc: {0}".format(len(data.loc[data['glucose_conc'] == 0])))
+print("number of rows missing glucose_conc: {0}".format(len(data.loc[data['glucose_conc'] == 0])))
+print("number of rows missing diastolic_bp: {0}".format(len(data.loc[data['diastolic_bp'] == 0])))
+print("number of rows missing insulin: {0}".format(len(data.loc[data['insulin'] == 0])))
+print("number of rows missing bmi: {0}".format(len(data.loc[data['bmi'] == 0])))
+print("number of rows missing diab_pred: {0}".format(len(data.loc[data['diab_pred'] == 0])))
+print("number of rows missing age: {0}".format(len(data.loc[data['age'] == 0])))
+print("number of rows missing skin: {0}".format(len(data.loc[data['skin'] == 0])))
+
+
+from sklearn.impute import SimpleImputer
+
+fill_values = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+X_train = fill_values.fit_transform(X_train)
+X_test = fill_values.fit_transform(X_test)
+
+## Apply Algorithm
+
+from sklearn.ensemble import RandomForestClassifier
+random_forest_model = RandomForestClassifier(random_state=10)
+
+random_forest_model.fit(X_train, y_train.ravel())
+
+predict_train_data = random_forest_model.predict(X_test)
+
+from sklearn import metrics
+# soall
+print("Akurasi : ", metrics.accuracy_score(y_test,predict_train_data)*100)
+
+# evaluasi menggunakan confussion matrix
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_test,predict_train_data)
+
+
+## Hyper Parameter Optimization
+
+params={
+ "learning_rate"    : [0.05, 0.10, 0.15, 0.20, 0.25, 0.30 ] ,
+ "max_depth"        : [ 3, 4, 5, 6, 8, 10, 12, 15],
+ "min_child_weight" : [ 1, 3, 5, 7 ],
+ "gamma"            : [ 0.0, 0.1, 0.2 , 0.3, 0.4 ],
+ "colsample_bytree" : [ 0.3, 0.4, 0.5 , 0.7 ]
+    
+}
+
+## Hyperparameter optimization using RandomizedSearchCV
+# from sklearn.model_selection import RandomizedSearchCV
+# import xgboost
+# classifier=xgboost.XGBClassifier()
+# random_search=RandomizedSearchCV(classifier,param_distributions=params,n_iter=5,scoring='roc_auc',n_jobs=-1,cv=5,verbose=3)
+# def timer(start_time=None):
+#     if not start_time:
+#         start_time = datetime.now()
+#         return start_time
+#     elif start_time:
+#         thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
+#         tmin, tsec = divmod(temp_sec, 60)
+#         print('\n Time taken: %i hours %i minutes and %s seconds.' % (thour, tmin, round(tsec, 2)))
+
+# from datetime import datetime
+# start_time = timer(None) # timing starts from this point for "start_time" variable
+# random_search.fit(X,y.ravel())
+# print(timer(start_time)) # timing ends here for "start_time" variable
+
+# random_search.best_estimator_
+
+# classifier=xgboost.XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#        colsample_bytree=0.3, gamma=0.0, learning_rate=0.25,
+#        max_delta_step=0, max_depth=3, min_child_weight=7, missing=None,
+#        n_estimators=100, n_jobs=1, nthread=None,
+#        objective='binary:logistic', random_state=0, reg_alpha=0,
+#        reg_lambda=1, scale_pos_weight=1, seed=None, silent=True,
+#        subsample=1)
+
+# from sklearn.model_selection import cross_val_score
+# score=cross_val_score(classifier,X,y.ravel(),cv=10)
+
+# print(score)
+# print(score.mean())
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+from six import StringIO
+from IPython.display import Image
+import pydotplus
+
+features = feature_columns
+features
+
+model = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+model = model.fit(X_train,y_train)
+y_pred = model.predict(X_test)
+print("Akurasi : ", metrics.accuracy_score(y_test,y_pred)*100)
+
+
+dot_data = StringIO()
+export_graphviz(model, out_file=dot_data, filled=True, rounded=True, special_characters=True, feature_names = features,class_names=['0','1'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('diabetes_predicition.png')
+Image(graph.create_png())
